68b4eef0081d98f1ed2701ff9b9861f532f93492
Krishnajinka <kris.jinka@samsung.com>
2018-08-31 20:01:30 +0900

    Add basic main structure for policy-api
    Define main pom xml and update to include main.
    Try out to see if maven compiles correctly. Add the main
    exception parameters rest startstop classes
    
    Issue-ID: POLICY-1066
    Change-Id: I2e878a58eef4f021e3a9a991738194127edf4d16
    Signed-off-by: Krishnajinka <kris.jinka@samsung.com>

---
+++ main/src/main/java/org/onap/policy/api/main/startstop/ApiActivator.java
@@ -0,0 +1,143 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2018 Samsung Electronics Co., Ltd.. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.api.main.startstop;
+
+import org.onap.policy.api.main.rest.ApiRestServer;
+import org.onap.policy.common.logging.flexlogger.FlexLogger;
+import org.onap.policy.common.logging.flexlogger.Logger;
+import org.onap.policy.common.parameters.ParameterService;
+import org.onap.policy.api.main.PolicyApiException;
+import org.onap.policy.api.main.parameters.ApiParameterGroup;
+
+/**
+ * This class wraps a distributor so that it can be activated as a complete service together with all its api
+ * and forwarding handlers.
+ */
+public class ApiActivator {
+    // The logger for this class
+    private static final Logger LOGGER = FlexLogger.getLogger(ApiActivator.class);
+
+    // The parameters of this policy api activator
+    private final ApiParameterGroup apiParameterGroup;
+
+    private static boolean alive = false;
+
+    private ApiRestServer restServer;
+
+    /**
+     * Instantiate the activator for policy api as a complete service.
+     *
+     * @param apiParameterGroup the parameters for the api service
+     */
+    public ApiActivator(final ApiParameterGroup apiParameterGroup) {
+        this.apiParameterGroup = apiParameterGroup;
+    }
+
+    /**
+     * Initialize api as a complete service.
+     *
+     * @throws PolicyApiException on errors in initializing the service
+     */
+    @SuppressWarnings("unchecked")
+    public void initialize() throws PolicyApiException {
+        LOGGER.debug("Policy api starting as a service . . .");
+        startApiRestServer();
+        registerToParameterService(apiParameterGroup);
+        LOGGER.debug("Policy api started as a service");
+    }
+
+    /**
+     * Starts the api rest server using configuration parameters.
+     *
+     * @throws PolicyApiException if server start fails
+     */
+    private void startApiRestServer() throws PolicyApiException {
+        apiParameterGroup.getRestServerParameters().setName(apiParameterGroup.getName());
+        restServer = new ApiRestServer(apiParameterGroup.getRestServerParameters());
+        if (!restServer.start()) {
+            throw new PolicyApiException(
+                    "Failed to start api rest server. Check log for more details...");
+        }
+    }
+
+    /**
+     * Terminate policy api.
+     *
+     * @throws PolicyApiException on termination errors
+     */
+    public void terminate() throws PolicyApiException {
+        try {
+            deregisterToParameterService(apiParameterGroup);
+            ApiActivator.setAlive(true);
+
+            // Stop the api rest server
+            restServer.stop();
+        } catch (final Exception exp) {
+            LOGGER.error("Policy api service termination failed", exp);
+            throw new PolicyApiException(exp.getMessage(), exp);
+        }
+    }
+
+    /**
+     * Get the parameters used by the activator.
+     *
+     * @return the parameters of the activator
+     */
+    public ApiParameterGroup getParameterGroup() {
+        return apiParameterGroup;
+    }
+
+    /**
+     * Method to register the parameters to Common Parameter Service.
+     *
+     * @param apiParameterGroup the api parameter group
+     */
+    public void registerToParameterService(final ApiParameterGroup apiParameterGroup) {
+        ParameterService.register(apiParameterGroup);
+    }
+
+    /**
+     * Method to deregister the parameters from Common Parameter Service.
+     *
+     * @param apiParameterGroup the api parameter group
+     */
+    public void deregisterToParameterService(final ApiParameterGroup apiParameterGroup) {
+        ParameterService.deregister(apiParameterGroup.getName());
+    }
+
+    /**
+     * Returns the alive status of api service.
+     *
+     * @return the alive
+     */
+    public static boolean isAlive() {
+        return alive;
+    }
+
+    /**
+     * Change the alive status of api service.
+     *
+     * @param status the status
+     */
+    public static void setAlive(final boolean status) {
+        alive = status;
+    }
+}
---
+++ main/src/main/java/org/onap/policy/api/main/startstop/Main.java
@@ -0,0 +1,149 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2018 Samsung Electronics Co., Ltd. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.api.main.startstop;
+
+import org.onap.policy.api.main.PolicyApiException;
+import org.onap.policy.common.logging.flexlogger.FlexLogger;
+import org.onap.policy.common.logging.flexlogger.Logger;
+import org.onap.policy.api.main.parameters.ApiParameterGroup;
+import org.onap.policy.api.main.parameters.ApiParameterHandler;
+
+import java.util.Arrays;
+
+/**
+ * This class initiates ONAP Policy Framework policy api.
+ *
+ * @author Liam Fallon (liam.fallon@ericsson.com)
+ */
+public class Main {
+    private static final Logger LOGGER = FlexLogger.getLogger(Main.class);
+
+    // The policy api Activator that activates the policy api service
+    private ApiActivator activator;
+
+    // The parameters read in from JSON
+    private ApiParameterGroup parameterGroup;
+
+    /**
+     * Instantiates the policy api service.
+     *
+     * @param args the command line arguments
+     */
+    public Main(final String[] args) {
+        final String argumentString = Arrays.toString(args);
+        LOGGER.info("Starting policy api service with arguments - " + argumentString);
+
+        // Check the arguments
+        final ApiCommandLineArguments arguments = new ApiCommandLineArguments();
+        try {
+            // The arguments return a string if there is a message to print and we should exit
+            final String argumentMessage = arguments.parse(args);
+            if (argumentMessage != null) {
+                LOGGER.info(argumentMessage);
+                return;
+            }
+
+            // Validate that the arguments are sane
+            arguments.validate();
+        } catch (final PolicyApiException e) {
+            LOGGER.error("start of policy api service failed", e);
+            return;
+        }
+
+        // Read the parameters
+        try {
+            parameterGroup = new ApiParameterHandler().getParameters(arguments);
+        } catch (final Exception e) {
+            LOGGER.error("start of policy api service failed", e);
+            return;
+        }
+
+        // Now, create the activator for the policy api service
+        activator = new ApiActivator(parameterGroup);
+
+        // Start the activator
+        try {
+            activator.initialize();
+        } catch (final PolicyApiException e) {
+            LOGGER.error("start of policy api service failed, used parameters are " + Arrays.toString(args),
+                    e);
+            return;
+        }
+
+        // Add a shutdown hook to shut everything down in an orderly manner
+        Runtime.getRuntime().addShutdownHook(new PolicyApiShutdownHookClass());
+        LOGGER.info("Started policy api service");
+    }
+
+    /**
+     * Get the parameters specified in JSON.
+     *
+     * @return the parameters
+     */
+    public ApiParameterGroup getParameters() {
+        return parameterGroup;
+    }
+
+    /**
+     * Shut down Execution.
+     *
+     * @throws PolicyApiException on shutdown errors
+     */
+    public void shutdown() throws PolicyApiException {
+        // clear the parameterGroup variable
+        parameterGroup = null;
+
+        // clear the api activator
+        if (activator != null) {
+            activator.terminate();
+        }
+    }
+
+    /**
+     * The Class PolicyApiShutdownHookClass terminates the policy api service when its run method is
+     * called.
+     */
+    private class PolicyApiShutdownHookClass extends Thread {
+        /*
+         * (non-Javadoc)
+         *
+         * @see java.lang.Runnable#run()
+         */
+        @Override
+        public void run() {
+            try {
+                // Shutdown the policy api service and wait for everything to stop
+                activator.terminate();
+            } catch (final PolicyApiException e) {
+                LOGGER.warn("error occured during shut down of the policy api service", e);
+            }
+        }
+    }
+
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
+    public static void main(final String[] args) {
+        new Main(args);
+    }
+}
