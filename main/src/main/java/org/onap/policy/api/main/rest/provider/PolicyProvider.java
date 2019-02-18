ea5933e10be66ef40e9d91ec93e4dbe0c45f1e70
Chenfei Gao <cgao@research.att.com>
2019-02-18 16:28:54 -0500

    Add more endpoints and swagger annotations
    Includes:
    a) Organized swagger models into new rest.model package
    b) Organized provider classes into new rest.provider package
    c) Added policyType GET/POST/DELETE endpoints
    d) Added policy GET/POST/DELETE endpoints
    e) Added swagger annotations for the new endpoints
    
    Issue-ID: POLICY-1515
    Change-Id: Ia159a4cfebdad36cb1b4a5cc6456d07d2fde09ee
    Signed-off-by: Chenfei Gao <cgao@research.att.com>

---
+++ main/src/main/java/org/onap/policy/api/main/rest/provider/PolicyProvider.java
@@ -0,0 +1,81 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP Policy API
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.api.main.rest.provider;
+
+import org.onap.policy.model.tosca.ToscaPolicy;
+import org.onap.policy.model.tosca.ToscaPolicyList;
+
+/**
+ * Class to provide all kinds of policy operations.
+ */
+public class PolicyProvider {
+
+    private static final String POST_OK = "Successfully created";
+    private static final String DELETE_OK = "Successfully deleted";
+
+    /**
+     * Retrieves a list of policies matching specified ID and version of both policy type and policy.
+     *
+     * @param policyTypeId the ID of policy type
+     * @param policyTypeVersion the version of policy type
+     * @param policyId the ID of policy
+     * @param policyVersion the version of policy
+     *
+     * @return the ToscaPolicyList object containing a list of policies matching specified fields
+     */
+    public ToscaPolicyList fetchPolicies(String policyTypeId, String policyTypeVersion,
+                                         String policyId, String policyVersion) {
+        // placeholder
+        return new ToscaPolicyList();
+    }
+
+    /**
+     * Creates a new policy for a policy type ID and version.
+     *
+     * @param policyTypeId the ID of policy type
+     * @param policyTypeVersion the version of policy type
+     * @param body the entity body of policy
+     *
+     * @return a string message indicating the operation results
+     */
+    public String createPolicy(String policyTypeId, String policyTypeVersion, ToscaPolicy body) {
+        // placeholder
+        return POST_OK;
+    }
+
+    /**
+     * Deletes the policies matching specified ID and version of both policy type and policy.
+     *
+     * @param policyTypeId the ID of policy type
+     * @param policyTypeVersion the version of policy type
+     * @param policyId the ID of policy
+     * @param policyVersion the version of policy
+     *
+     * @return a string message indicating the operation results
+     */
+    public String deletePolicies(String policyTypeId, String policyTypeVersion,
+                                 String policyId, String policyVersion) {
+        // placeholder
+        return DELETE_OK;
+    }
+}
--- main/src/test/java/org/onap/policy/api/main/rest/TestApiRestServer.java
+++ main/src/test/java/org/onap/policy/api/main/rest/TestApiRestServer.java
@@ -26,7 +26,15 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.Properties;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
@@ -35,6 +43,7 @@ import javax.ws.rs.core.MediaType;
 
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.junit.After;
 import org.junit.Test;
 import org.onap.policy.api.main.exception.PolicyApiException;
 import org.onap.policy.api.main.parameters.CommonTestData;
@@ -46,8 +55,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Class to perform unit test of HealthCheckMonitor.
+ * Class to perform unit test of {@link ApiRestServer}.
  *
+ * @author Chenfei Gao (cgao@research.att.com)
  */
 public class TestApiRestServer {
 
@@ -56,15 +66,37 @@ public class TestApiRestServer {
     private static final String ALIVE = "alive";
     private static final String SELF = "self";
     private static final String NAME = "Policy API";
+    private static final String HEALTHCHECK_ENDPOINT = "healthcheck";
+    private static final String STATISTICS_ENDPOINT = "statistics";
+    private static String KEYSTORE = System.getProperty("user.dir") + "/src/test/resources/ssl/policy-keystore";
+    private Main main;
+    private ApiRestServer restServer;
+
+    /**
+     * Method for cleanup after each test.
+     */
+    @After
+    public void teardown() {
+        try {
+            if (NetworkUtil.isTcpPortOpen("localhost", 6969, 1, 1000L)) {
+                if (main != null) {
+                    stopApiService(main);
+                } else if (restServer != null) {
+                    restServer.stop();
+                }
+            }
+        } catch (InterruptedException | IOException | PolicyApiException exp) {
+            LOGGER.error("teardown failed", exp);
+        }
+    }
 
     @Test
-    public void testHealthCheckSuccess() throws PolicyApiException, InterruptedException {
-        final String reportString = "Report [name=Policy API, url=self, healthy=true, code=200, message=alive]";
+    public void testHealthCheckSuccess() {
         try {
-            final Main main = startApiService();
-            final HealthCheckReport report = performHealthCheck();
-            validateReport(NAME, SELF, true, 200, ALIVE, reportString, report);
-            stopApiService(main);
+            main = startApiService(true);
+            final Invocation.Builder invocationBuilder = sendHttpRequest(HEALTHCHECK_ENDPOINT);
+            final HealthCheckReport report = invocationBuilder.get(HealthCheckReport.class);
+            validateHealthCheckReport(NAME, SELF, true, 200, ALIVE, report);
         } catch (final Exception exp) {
             LOGGER.error("testHealthCheckSuccess failed", exp);
             fail("Test should not throw an exception");
@@ -72,26 +104,109 @@ public class TestApiRestServer {
     }
 
     @Test
-    public void testHealthCheckFailure() throws InterruptedException {
-        final String reportString = "Report [name=Policy API, url=self, healthy=false, code=500, message=not alive]";
+    public void testHealthCheckFailure() throws InterruptedException, IOException {
         final RestServerParameters restServerParams = new CommonTestData().getRestServerParameters(false);
         restServerParams.setName(CommonTestData.API_GROUP_NAME);
-        final ApiRestServer restServer = new ApiRestServer(restServerParams);
+        restServer = new ApiRestServer(restServerParams);
         try {
             restServer.start();
-            final HealthCheckReport report = performHealthCheck();
-            validateReport(NAME, SELF, false, 500, NOT_ALIVE, reportString, report);
+            final Invocation.Builder invocationBuilder = sendHttpRequest(HEALTHCHECK_ENDPOINT);
+            final HealthCheckReport report = invocationBuilder.get(HealthCheckReport.class);
+            validateHealthCheckReport(NAME, SELF, false, 500, NOT_ALIVE, report);
             assertTrue(restServer.isAlive());
             assertTrue(restServer.toString().startsWith("ApiRestServer [servers="));
-            restServer.shutdown();
         } catch (final Exception exp) {
             LOGGER.error("testHealthCheckFailure failed", exp);
             fail("Test should not throw an exception");
         }
     }
 
-    private Main startApiService() {
-        final String[] apiConfigParameters = { "-c", "parameters/ApiConfigParameters.json" };
+    @Test
+    public void testHttpsHealthCheckSuccess() {
+        try {
+            main = startApiService(false);
+            final Invocation.Builder invocationBuilder = sendHttpsRequest(HEALTHCHECK_ENDPOINT);
+            final HealthCheckReport report = invocationBuilder.get(HealthCheckReport.class);
+            validateHealthCheckReport(NAME, SELF, true, 200, ALIVE, report);
+        } catch (final Exception exp) {
+            LOGGER.error("testHttpsHealthCheckSuccess failed", exp);
+            fail("Test should not throw an exception");
+        }
+    }
+
+    @Test
+    public void testApiStatistics_200() {
+        try {
+            main = startApiService(true);
+            Invocation.Builder invocationBuilder = sendHttpRequest(STATISTICS_ENDPOINT);
+            StatisticsReport report = invocationBuilder.get(StatisticsReport.class);
+            validateStatisticsReport(report, 0, 200);
+            updateApiStatistics();
+            invocationBuilder = sendHttpRequest(STATISTICS_ENDPOINT);
+            report = invocationBuilder.get(StatisticsReport.class);
+            validateStatisticsReport(report, 1, 200);
+            ApiStatisticsManager.resetAllStatistics();
+        } catch (final Exception exp) {
+            LOGGER.error("testApiStatistics_200 failed", exp);
+            fail("Test should not throw an exception");
+        }
+    }
+
+    @Test
+    public void testApiStatistics_500() {
+        final RestServerParameters restServerParams = new CommonTestData().getRestServerParameters(false);
+        restServerParams.setName(CommonTestData.API_GROUP_NAME);
+        restServer = new ApiRestServer(restServerParams);
+        try {
+            restServer.start();
+            final Invocation.Builder invocationBuilder = sendHttpRequest(STATISTICS_ENDPOINT);
+            final StatisticsReport report = invocationBuilder.get(StatisticsReport.class);
+            validateStatisticsReport(report, 0, 500);
+            ApiStatisticsManager.resetAllStatistics();
+        } catch (final Exception exp) {
+            LOGGER.error("testApiStatistics_500 failed", exp);
+            fail("Test should not throw an exception");
+        }
+    }
+
+    @Test
+    public void testHttpsApiStatistics() {
+        try {
+            main = startApiService(false);
+            final Invocation.Builder invocationBuilder = sendHttpsRequest(STATISTICS_ENDPOINT);
+            final StatisticsReport report = invocationBuilder.get(StatisticsReport.class);
+            validateStatisticsReport(report, 0, 200);
+        } catch (final Exception exp) {
+            LOGGER.error("testHttpsApiStatistics failed", exp);
+            fail("Test should not throw an exception");
+        }
+    }
+
+    @Test
+    public void testApiStatisticsConstructorIsPrivate() {
+        try {
+            final Constructor<ApiStatisticsManager> constructor = ApiStatisticsManager.class.getDeclaredConstructor();
+            assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+            constructor.setAccessible(true);
+            constructor.newInstance();
+        } catch (final Exception exp) {
+            assertTrue(exp.getCause().toString().contains("Instantiation of the class is not allowed"));
+        }
+    }
+
+    private Main startApiService(final boolean http) {
+        final String[] apiConfigParameters = new String[2];
+        if (http) {
+            apiConfigParameters[0] = "-c";
+            apiConfigParameters[1] = "parameters/ApiConfigParameters.json";
+        } else {
+            final Properties systemProps = System.getProperties();
+            systemProps.put("javax.net.ssl.keyStore", KEYSTORE);
+            systemProps.put("javax.net.ssl.keyStorePassword", "Pol1cy_0nap");
+            System.setProperties(systemProps);
+            apiConfigParameters[0] = "-c";
+            apiConfigParameters[1] = "parameters/ApiConfigParameters_Https.json";
+        }
         return new Main(apiConfigParameters);
     }
 
@@ -99,30 +214,100 @@ public class TestApiRestServer {
         main.shutdown();
     }
 
-    private HealthCheckReport performHealthCheck() throws InterruptedException, IOException {
+    private Invocation.Builder sendHttpRequest(final String endpoint) throws Exception {
         final ClientConfig clientConfig = new ClientConfig();
 
         final HttpAuthenticationFeature feature = HttpAuthenticationFeature.basic("healthcheck", "zb!XztG34");
         clientConfig.register(feature);
 
         final Client client = ClientBuilder.newClient(clientConfig);
-        final WebTarget webTarget = client.target("http://localhost:6969/healthcheck");
+        final WebTarget webTarget = client.target("http://localhost:6969/policy/api/v1/" + endpoint);
 
         final Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
-        
+
         if (!NetworkUtil.isTcpPortOpen("localhost", 6969, 6, 10000L)) {
             throw new IllegalStateException("cannot connect to port 6969");
         }
-        return invocationBuilder.get(HealthCheckReport.class);
+        return invocationBuilder;
     }
 
-    private void validateReport(final String name, final String url, final boolean healthy, final int code,
-            final String message, final String reportString, final HealthCheckReport report) {
+    private Invocation.Builder sendHttpsRequest(final String endpoint) throws Exception {
+
+        final TrustManager[] noopTrustManager = new TrustManager[] { new X509TrustManager() {
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+
+            @Override
+            public void checkClientTrusted(final java.security.cert.X509Certificate[] certs, final String authType) {}
+
+            @Override
+            public void checkServerTrusted(final java.security.cert.X509Certificate[] certs, final String authType) {}
+        } };
+
+        final SSLContext sc = SSLContext.getInstance("TLSv1.2");
+        sc.init(null, noopTrustManager, new SecureRandom());
+        final ClientBuilder clientBuilder = ClientBuilder.newBuilder().sslContext(sc).hostnameVerifier((host,
+                session) -> true);
+        final Client client = clientBuilder.build();
+        final HttpAuthenticationFeature feature = HttpAuthenticationFeature.basic("healthcheck", "zb!XztG34");
+        client.register(feature);
+
+        final WebTarget webTarget = client.target("https://localhost:6969/policy/api/v1/" + endpoint);
+
+        final Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+
+        if (!NetworkUtil.isTcpPortOpen("localhost", 6969, 6, 10000L)) {
+            throw new IllegalStateException("cannot connect to port 6969");
+        }
+        return invocationBuilder;
+    }
+
+    private void updateApiStatistics() {
+        ApiStatisticsManager.updateTotalApiCallCount();
+        ApiStatisticsManager.updateApiCallSuccessCount();
+        ApiStatisticsManager.updateApiCallFailureCount();
+        ApiStatisticsManager.updateTotalPolicyGetCount();
+        ApiStatisticsManager.updateTotalPolicyPostCount();
+        ApiStatisticsManager.updateTotalPolicyTypeGetCount();
+        ApiStatisticsManager.updateTotalPolicyTypePostCount();
+        ApiStatisticsManager.updatePolicyGetSuccessCount();
+        ApiStatisticsManager.updatePolicyGetFailureCount();
+        ApiStatisticsManager.updatePolicyPostSuccessCount();
+        ApiStatisticsManager.updatePolicyPostFailureCount();
+        ApiStatisticsManager.updatePolicyTypeGetSuccessCount();
+        ApiStatisticsManager.updatePolicyTypeGetFailureCount();
+        ApiStatisticsManager.updatePolicyTypePostSuccessCount();
+        ApiStatisticsManager.updatePolicyTypePostFailureCount();
+    }
+
+    private void validateStatisticsReport(final StatisticsReport report, final int count, final int code) {
+        assertEquals(code, report.getCode());
+        assertEquals(count, report.getTotalApiCallCount());
+        assertEquals(count, report.getApiCallSuccessCount());
+        assertEquals(count, report.getApiCallFailureCount());
+        assertEquals(count, report.getTotalPolicyGetCount());
+        assertEquals(count, report.getTotalPolicyPostCount());
+        assertEquals(count, report.getTotalPolicyTypeGetCount());
+        assertEquals(count, report.getTotalPolicyTypePostCount());
+        assertEquals(count, report.getPolicyGetSuccessCount());
+        assertEquals(count, report.getPolicyGetFailureCount());
+        assertEquals(count, report.getPolicyPostSuccessCount());
+        assertEquals(count, report.getPolicyPostFailureCount());
+        assertEquals(count, report.getPolicyTypeGetSuccessCount());
+        assertEquals(count, report.getPolicyTypeGetFailureCount());
+        assertEquals(count, report.getPolicyTypePostSuccessCount());
+        assertEquals(count, report.getPolicyTypePostFailureCount());
+    }
+
+    private void validateHealthCheckReport(final String name, final String url, final boolean healthy, final int code,
+            final String message, final HealthCheckReport report) {
         assertEquals(name, report.getName());
         assertEquals(url, report.getUrl());
         assertEquals(healthy, report.isHealthy());
         assertEquals(code, report.getCode());
         assertEquals(message, report.getMessage());
-        assertEquals(reportString, report.toString());
     }
 }
