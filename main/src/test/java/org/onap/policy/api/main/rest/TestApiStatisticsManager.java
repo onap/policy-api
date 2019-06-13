db27731b36f50bfe731fd33e96f2c3ce34f659e0
Sandeep J <sandeejh@in.ibm.com>
2019-06-13 12:47:39 +0530

    added file to test ApiStatisticsManager.java
    to increase code coverage
    
    Issue-ID: POLICY-1791
    Change-Id: Ie0a5027ad61224c44b30127348c4a81454549fc9
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

---
+++ main/src/test/java/org/onap/policy/api/main/rest/TestApiStatisticsManager.java
@@ -0,0 +1,45 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP Policy API
+ * ================================================================================
+ * Copyright (C) 2019 IBM.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.api.main.rest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestApiStatisticsManager {
+
+    @Before
+    public void setUp() {
+        ApiStatisticsManager.resetAllStatistics();
+    }
+
+    @Test
+    public void testUpdateMethods() {
+        assertEquals(1, ApiStatisticsManager.updateTotalPolicyTypePostCount());
+        assertEquals(1, ApiStatisticsManager.updatePolicyGetSuccessCount());
+        assertEquals(1, ApiStatisticsManager.updatePolicyPostSuccessCount());
+        assertEquals(1, ApiStatisticsManager.updatePolicyTypePostFailureCount());
+    }
+
+}
