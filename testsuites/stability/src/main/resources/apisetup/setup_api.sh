eed809d068527c939e00b31d28faafab3e885995
Chenfei Gao <cgao@research.att.com>
2019-07-12 21:54:07 -0400

    Add policy api stability test
    Add jmeter test plan for api stability test
    Add scripts and config files for setting up api docker
    Add scripts and config files for setting up other components
    
    Issue-ID: POLICY-1805
    Change-Id: Ia06a3834ddb9c0ca6ed78a5b1e941f2b4e372dda
    Signed-off-by: Chenfei Gao <cgao@research.att.com>

---
+++ testsuites/stability/src/main/resources/apisetup/setup_api.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# ============LICENSE_START=======================================================
+#  Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# ============LICENSE_END=========================================================
+
+# the directory of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo ${DIR}
+docker run -p 9090:9090 -p 6969:6969 -v ${DIR}/config/api/bin/policy-api.sh:/opt/app/policy/api/bin/policy-api.sh -v ${DIR}/config/api/etc/defaultConfig.json:/opt/app/policy/api/etc/defaultConfig.json --add-host mariadb:10.2.0.41 --name policy-api -d --rm nexus3.onap.org:10001/onap/policy-api:2.1.1-SNAPSHOT-latest
