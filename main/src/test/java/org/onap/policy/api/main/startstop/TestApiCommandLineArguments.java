2187558aa7eac99f5afe9232354a6c7af0e9baf3
Jim Hahn <jrh3@att.com>
2019-08-28 09:53:19 -0400

    Preload control loop coordination type in API
    Modified json based on input from Chenfei.
    
    Change-Id: Ibd70c5737713fee5dcd2370e02ef5f567d2a73b3
    Issue-ID: POLICY-1847
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- main/src/test/java/org/onap/policy/api/main/startstop/TestApiCommandLineArguments.java
+++ main/src/test/java/org/onap/policy/api/main/startstop/TestApiCommandLineArguments.java
@@ -43,7 +43,7 @@ public class TestApiCommandLineArguments {
     @Test(expected = PolicyApiRuntimeException.class)
     public void testApiCommandLineArgumentsStringArray() {
         String [] args = {"---d"};
-        ApiCommandLineArguments apiCmdArgs1 = new ApiCommandLineArguments(args);
+        new ApiCommandLineArguments(args);
     }
 
     @Test
