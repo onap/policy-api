e54717e7b2f07e8d0f98ca16382d3c9c47ff9a40
Rashmi Pujar <rashmi.pujar1@bell.ca>
2022-02-22 01:39:00 -0500

    Spring repository and service layer for policy-api
    - Add the spring repository and service layers to policy-api.
    - Unit tests are modified to use the spring service layers
    
    Next-up: Migrate the usage of policy-models-provider in policy-api
    to spring boot based services to talk to database (POLICY-3924)
    
    Issue-ID: POLICY-3923
    Signed-off-by: Rashmi Pujar <rashmi.pujar1@bell.ca>
    Change-Id: Ib6840040b32f24f019da802d3b246dab1bfccbe3

---
+++ main/src/main/java/org/onap/policy/api/main/service/PdpGroupService.java
@@ -0,0 +1,92 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *   Copyright (C) 2022 Bell Canada. All rights reserved.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================;
+ */
+
+package org.onap.policy.api.main.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.ws.rs.core.Response;
+import lombok.RequiredArgsConstructor;
+import org.onap.policy.api.main.repository.PdpGroupRepository;
+import org.onap.policy.models.base.PfModelRuntimeException;
+import org.onap.policy.models.pdp.concepts.PdpGroup;
+import org.onap.policy.models.pdp.concepts.PdpSubGroup;
+import org.onap.policy.models.pdp.persistence.concepts.JpaPdpGroup;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaConceptIdentifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class PdpGroupService {
+
+    private final PdpGroupRepository pdpGroupRepository;
+
+    /**
+     * Fetch all the PDP groups from the DB.
+     * @return a list of {@link PdpGroup}
+     */
+    private List<PdpGroup> getAllPdpGroups() {
+        return pdpGroupRepository.findAll().stream().map(JpaPdpGroup::toAuthorative).collect(Collectors.toList());
+    }
+
+    /**
+     * Assert that the policy type is not supported in any PDP group.
+     *
+     * @param policyTypeName the policy type name
+     * @param policyTypeVersion the policy type version
+     * @throws PfModelRuntimeException if the policy type is supported in a PDP group
+     */
+    public void assertPolicyTypeNotSupportedInPdpGroup(final String policyTypeName, final String policyTypeVersion)
+        throws PfModelRuntimeException {
+        final var policyTypeIdentifier = new ToscaConceptIdentifier(policyTypeName, policyTypeVersion);
+        for (PdpGroup pdpGroup : getAllPdpGroups()) {
+            for (PdpSubGroup pdpSubGroup : pdpGroup.getPdpSubgroups()) {
+                if (pdpSubGroup.getSupportedPolicyTypes().contains(policyTypeIdentifier)) {
+                    throw new PfModelRuntimeException(Response.Status.NOT_ACCEPTABLE,
+                        "policy type is in use, it is referenced in PDP group " + pdpGroup.getName() + " subgroup "
+                            + pdpSubGroup.getPdpType());
+                }
+            }
+        }
+    }
+
+    /**
+     * Assert that the policy is not deployed in a PDP group.
+     *
+     * @param policyName the policy name
+     * @param policyVersion the policy version
+     * @throws PfModelRuntimeException thrown if the policy is deployed in a PDP group
+     */
+    public void assertPolicyNotDeployedInPdpGroup(final String policyName, final String policyVersion)
+        throws PfModelRuntimeException {
+        final var policyIdentifier = new ToscaConceptIdentifier(policyName, policyVersion);
+        for (PdpGroup pdpGroup : getAllPdpGroups()) {
+            for (PdpSubGroup pdpSubGroup : pdpGroup.getPdpSubgroups()) {
+                if (pdpSubGroup.getPolicies().contains(policyIdentifier)) {
+                    throw new PfModelRuntimeException(Response.Status.NOT_ACCEPTABLE,
+                        "policy is in use, it is deployed in PDP group " + pdpGroup.getName() + " subgroup "
+                            + pdpSubGroup.getPdpType());
+                }
+            }
+        }
+    }
+}
---
+++ main/src/test/java/org/onap/policy/api/main/service/TestPdpGroupService.java
@@ -0,0 +1,84 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *   Copyright (C) 2022 Bell Canada. All rights reserved.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================;
+ */
+
+package org.onap.policy.api.main.service;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.onap.policy.api.main.repository.PdpGroupRepository;
+import org.onap.policy.common.utils.coder.CoderException;
+import org.onap.policy.common.utils.coder.StandardCoder;
+import org.onap.policy.common.utils.resources.ResourceUtils;
+import org.onap.policy.models.pdp.concepts.PdpGroups;
+import org.onap.policy.models.pdp.persistence.concepts.JpaPdpGroup;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestPdpGroupService {
+
+    @Mock
+    private PdpGroupRepository pdpGroupRepository;
+
+    @InjectMocks
+    private PdpGroupService pdpGroupService;
+
+    /**
+     * Test setup.
+     * @throws CoderException decode errors
+     */
+    @Before
+    public void setUp() throws CoderException {
+        var pdpGroups = new StandardCoder().decode(ResourceUtils.getResourceAsString("pdpgroups/PdpGroups.json"),
+            PdpGroups.class).getGroups();
+        List<JpaPdpGroup> jpaPdpGroupList = new ArrayList<>();
+        pdpGroups.forEach(pdpGroup -> jpaPdpGroupList.add(new JpaPdpGroup(pdpGroup)));
+
+        when(pdpGroupRepository.findAll()).thenReturn(jpaPdpGroupList);
+    }
+
+    @Test
+    public void testAssertPolicyTypeNotSupportedInPdpGroup() {
+        assertThatCode(() -> pdpGroupService.assertPolicyTypeNotSupportedInPdpGroup("policy_type_not_supported",
+            "1.0.0")).doesNotThrowAnyException();
+
+        assertThatThrownBy(() -> pdpGroupService.assertPolicyTypeNotSupportedInPdpGroup(
+            "onap.policies.controlloop.guard.common.FrequencyLimiter", "1.0.0"))
+            .hasMessage("policy type is in use, it is referenced in PDP group defaultGroup subgroup xacml");
+    }
+
+    @Test
+    public void testAssertPolicyNotDeployedInPdpGroup() {
+        assertThatCode(() -> pdpGroupService.assertPolicyNotDeployedInPdpGroup("policy_not_deployed", "1.0.0"))
+            .doesNotThrowAnyException();
+
+        assertThatThrownBy(() -> pdpGroupService.assertPolicyNotDeployedInPdpGroup(
+            "onap.policies.controlloop.operational.common.apex.SampleDomain", "1.0.0"))
+            .hasMessage("policy is in use, it is deployed in PDP group defaultGroup subgroup apex");
+    }
+}
