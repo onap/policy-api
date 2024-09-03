9164ef359603b40091245797148ce43cf758e1f5
adheli.tavares <adheli.tavares@est.tech>
2024-09-03 11:10:55 +0100

    Fix small sonar issue
    Issue-ID: POLICY-5068
    Change-Id: I6735f63350fa93a1255b2c2713f92bf002382294
    Signed-off-by: adheli.tavares <adheli.tavares@est.tech>

--- main/src/test/java/org/onap/policy/api/main/service/TestToscaServiceTemplateServiceForPolicyTypeCrud.java
+++ main/src/test/java/org/onap/policy/api/main/service/TestToscaServiceTemplateServiceForPolicyTypeCrud.java
@@ -27,7 +27,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import jakarta.ws.rs.core.Response;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
